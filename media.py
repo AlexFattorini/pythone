b197cc29386c088f63975e665006f8d86ff49010
AlexFattorini <127417035+AlexFattorini@users.noreply.github.com>
2025-01-27 10:40:30 +0100

    media.py

--- media.py
+++ media.py
@@ -1,17 +1,14 @@
 somma = 0
 conteggio = 0
 
-while True:
-    domanda = input("Vuoi inserire un numero? (sì/no): ")
 
-    if domanda == "no":
-        break
+domanda = input("Vuoi inserire un numero? (sì/no): ")
+while domanda == "sì":
+    valore = float(input("Inserisci il numero: "))
+    somma += valore
+    conteggio += 1
 
-    if domanda == "sì":
-
-        valore = float(input("Inserisci il numero: "))
-        somma += valore
-        conteggio += 1
+    domanda = input("Vuoi inserire un altro numero? (sì/no): ")
 
 
 if conteggio > 0:
